ef0687d505e90d4d0e2781833db30dd145f7f09b
Polin <853450031@qq.com>
2019-11-07 19:06:35 +0800

    20191107Turbine集群监控

---
+++ src/main/java/com/micro/turbine/MicroTurbineApplication.java
@@ -0,0 +1,15 @@
+package com.micro.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+//http://127.0.0.1:8085/turbine.stream
+@EnableEurekaClient
+@EnableTurbine
+@SpringBootApplication
+public class MicroTurbineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTurbineApplication.class, args);
+	}
+}
