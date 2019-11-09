5e5a90757db9b5b123900870633e7e520c5168f9
Polin <853450031@qq.com>
2019-11-09 20:35:12 +0800

    update

--- src/main/java/com/micro/turbine/MicroTurbineApplication.java
+++ src/main/java/com/micro/turbine/MicroTurbineApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 //http://127.0.0.1:8085/turbine.stream
+//http://127.0.0.1:8085/turbine.stream?cluster=ribbon
+
 @EnableEurekaClient
 @EnableTurbine
 @SpringBootApplication
